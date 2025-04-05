63406ce0a0187507a044d24c35f025c84a496b73
zhaijineet <159569081+zhaijineet@users.noreply.github.com>
2025-04-05 17:10:16 +0800

    Update CatBurgerItem.java

--- src/main/java/net/zhaiji/catburger/item/CatBurgerItem.java
+++ src/main/java/net/zhaiji/catburger/item/CatBurgerItem.java
@@ -8,7 +8,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
