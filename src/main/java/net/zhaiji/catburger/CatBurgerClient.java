1049ca2e42bcffc453b4cae1ce85b6a81db55dd7
zhaijineet <159569081+zhaijineet@users.noreply.github.com>
2025-05-17 20:26:36 +0800

    fix YSMCompat

--- src/main/java/net/zhaiji/catburger/CatBurgerClient.java
+++ src/main/java/net/zhaiji/catburger/CatBurgerClient.java
@@ -16,42 +16,41 @@ import net.zhaiji.catburger.init.InitItem;
 public class CatBurgerClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        TrinketRendererRegistry.registerRenderer(InitItem.CAT_BURGER, new CatBurgerRenderer());
-
-        if (YSMCompat.isLoad()) {
-            registerYSMCompatRenderer();
+        if (!YSMCompat.isLoad()) {
+            TrinketRendererRegistry.registerRenderer(InitItem.CAT_BURGER, new CatBurgerRenderer());
+//            registerYSMCompatRenderer();
         }
     }
 
-    private void registerYSMCompatRenderer() {
-        LivingEntityFeatureRendererRegistrationCallback.EVENT.register(
-                (entityType, entityRenderer, registrationHelper, context) -> {
-                    registrationHelper.register(new YSMCompatFeatureRenderer<>(entityRenderer));
-                }
-        );
-    }
-
-    private static class YSMCompatFeatureRenderer<T extends LivingEntity, M extends EntityModel<T>>
-            extends RenderLayer<T, M> {
-
-        public YSMCompatFeatureRenderer(RenderLayerParent<T, M> parent) {
-            super(parent);
-        }
-
-        @Override
-        public void render(
-                PoseStack poseStack,
-                MultiBufferSource multiBufferSource,
-                int light,
-                T livingEntity,
-                float limbAngle,
-                float limbDistance,
-                float tickDelta,
-                float animationProgress,
-                float headYaw,
-                float headPitch) {
-
-            YSMCompat.renderLivingPost(livingEntity, tickDelta, poseStack, multiBufferSource, light);
-        }
-    }
+//    private void registerYSMCompatRenderer() {
+//        LivingEntityFeatureRendererRegistrationCallback.EVENT.register(
+//                (entityType, entityRenderer, registrationHelper, context) -> {
+//                    registrationHelper.register(new YSMCompatFeatureRenderer<>(entityRenderer));
+//                }
+//        );
+//    }
+//
+//    private static class YSMCompatFeatureRenderer<T extends LivingEntity, M extends EntityModel<T>>
+//            extends RenderLayer<T, M> {
+//
+//        public YSMCompatFeatureRenderer(RenderLayerParent<T, M> parent) {
+//            super(parent);
+//        }
+//
+//        @Override
+//        public void render(
+//                PoseStack poseStack,
+//                MultiBufferSource multiBufferSource,
+//                int light,
+//                T livingEntity,
+//                float limbAngle,
+//                float limbDistance,
+//                float tickDelta,
+//                float animationProgress,
+//                float headYaw,
+//                float headPitch) {
+//
+//            YSMCompat.renderLivingPost(livingEntity, tickDelta, poseStack, multiBufferSource, light);
+//        }
+//    }
 }
